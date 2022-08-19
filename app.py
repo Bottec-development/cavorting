919697b50a25421e2a89e2830ce399fa1d53ef9a
TheXacx <mustepvbif@yandex.ru>
2022-08-19 23:57:02 +0300

    initial start bot

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ async def on_startup(dp):
 
     from utils.notify_admins import on_startup_notify
 
-    await on_startup_notify(dp)
+    #await on_startup_notify(dp)
     await set_default_commands(dp)
 
 
