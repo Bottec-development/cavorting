1fc4af09adc913cbeabdf6ff660b9369959b99b4
Alex <sashamarx67@gmail.com>
2022-08-22 15:10:05 +0300

    инициализировал новые хендлеры

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -3,5 +3,9 @@ from .meetingrooms import dp
 from .booking import dp
 from .event_space import dp
 from .how_to_get import dp
+from .parking import dp
+from .actions_manager_handler import dp
+from .chat_with_manager import dp
+from .support import dp
 
 __all__ = ["dp"]
