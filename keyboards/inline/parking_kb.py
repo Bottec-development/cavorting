4335bc1c4b02ca63b1ceeeba6fef5ad7312f0259
Alex <sashamarx67@gmail.com>
2022-08-22 15:12:36 +0300

    создал доп. клавиатуру для парковки

---
+++ keyboards/inline/parking_kb.py
@@ -0,0 +1,44 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from data.config import SITE_URL
+
+
+async def parking_keyboard(client_id):
+    parking = InlineKeyboardMarkup(row_width=3, inline_keyboard=
+    [
+        [
+            InlineKeyboardButton(text='Обнулить и оплатить', callback_data='pay_parking')
+        ],
+        [
+            InlineKeyboardButton(text='Обнулить (счет для компании)', callback_data=f'chat_parking_{client_id}')
+        ],
+        [
+            InlineKeyboardButton(text='В меню', callback_data='back_to_menu_callback')
+        ]
+    ])
+    return parking
+
+
+parking_site = InlineKeyboardMarkup(row_width=3, inline_keyboard=
+[
+    [
+        InlineKeyboardButton(text='Перейти', url=SITE_URL)
+    ],
+    [
+        InlineKeyboardButton(text='Назад', callback_data='parling')
+    ]
+])
+
+main_parking = InlineKeyboardMarkup(row_width=3, inline_keyboard=
+                                    [
+                                        [
+                                            InlineKeyboardButton(text='Назад.', callback_data='parling')
+                                        ]
+                                    ])
+
+go_menu = InlineKeyboardMarkup(row_width=3,
+                               inline_keyboard=[
+                                   [
+                                       InlineKeyboardButton(text='В меню', callback_data='back_to_menu_callback')
+                                   ]
+                               ])
